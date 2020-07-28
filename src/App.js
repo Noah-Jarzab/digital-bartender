9353df50e2c4cb4d112442984e11d8ad75eac664
Noah-Jarzab <njarzab721@gmail.com>
2020-07-28 12:49:32 -0600

    Added proposal.md and hid API key

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const apiKey = process.env.REACT_APP_MYAPI_KEY;
   return (
     <div className="App">
       <header className="App-header">
