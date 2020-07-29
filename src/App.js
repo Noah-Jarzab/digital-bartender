2a2ba64361320e9f2c80c81fa9d8dcfc3035f328
Noah-Jarzab <njarzab721@gmail.com>
2020-07-29 10:26:44 -0600

    Added the random cocktail functionality

--- src/App.js
+++ src/App.js
@@ -8,23 +8,48 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			cocktail: {},
+			cocktail: [],
 		};
 	}
 	componentDidMount() {
 		fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-		.then((res) => res.json())
-		.then((json) => {
-			this.setState({ cocktail: json });
-		})
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({ cocktail: json.drinks[0] });
+			});
 	}
 	render() {
+		console.log(this.state.cocktail)
 		const apiKey = process.env.REACT_APP_MYAPI_KEY;
 		return (
 			<div className='App'>
-				<header className='App-header'>
-					
-				</header>
+				<h1>{this.state.cocktail.strDrink}</h1>
+				<img
+					src={this.state.cocktail.strDrinkThumb}
+					alt={this.state.cocktail.strDrink}
+				/>
+				<ul>
+					<li>
+						{this.state.cocktail.strMeasure1}{' '}
+						{this.state.cocktail.strIngredient1}
+					</li>
+					<li>
+						{this.state.cocktail.strMeasure2}{' '}
+						{this.state.cocktail.strIngredient2}
+					</li>
+					<li>
+						{this.state.cocktail.strMeasure3}{' '}
+						{this.state.cocktail.strIngredient3}
+					</li>
+					<li>
+						{this.state.cocktail.strMeasure4}{' '}
+						{this.state.cocktail.strIngredient4}
+					</li>
+					<li>
+						{this.state.cocktail.strMeasure5}{' '}
+						{this.state.cocktail.strIngredient5}
+					</li>
+				</ul>
 			</div>
 		);
 	}
--- src/Cocktail.js
+++ src/Cocktail.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 class Cocktail extends Component {
     render() {
+        let result = this.props.data.find(
+            (cocktail) => cocktail.name === this.props.params.cocktail
+        );
+        console.log(this.props.match);
         return (
             <div>
-                
+                <img src={result.} alt={result.name}/>
+                <h2>{result.name}</h2>
             </div>
         );
     }
